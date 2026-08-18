dd841cb72c5ca797c3664cf7c949ae675dfd1a4d
mlelova <michaela.lelova@gmail.com>
2026-08-18 16:00:50 +0200

    .

--- visualisation/__init__.py
+++ visualisation/__init__.py
@@ -1,3 +1,5 @@
 from .visualisation import DashboardError, generate_dashboard
 
 __all__ = ["DashboardError", "generate_dashboard"]
+
+# python -m visualisation.cli ./small_scale_tests/baseline_2009_network/baseline_2030_cy2009_solved.nc -o ./OUTPUT_HTML.html --default-zone DE00 --title Title
